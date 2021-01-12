a0f34bf52a75f10322a33241aac2a13602a3313b
vivek <vivekdesai55@gmail.com>
2021-01-12 09:01:50 +0530

    Change is log example

--- dataProducer/src/main/scala/weatherDataProducer/exampleLogging.scala
+++ dataProducer/src/main/scala/weatherDataProducer/exampleLogging.scala
@@ -7,5 +7,8 @@ object exampleLogging extends App {
   val logger = LoggerFactory.getLogger(getClass.getName)
   println(logger.isInfoEnabled())
   logger.info("I am a log message")
+  logger.debug("I am a log message")
+  logger.error("I am a log message")
+  
   println("Hello, World!")
 }
